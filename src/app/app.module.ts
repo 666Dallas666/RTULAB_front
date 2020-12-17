8cb6f3a4c913837a6f20441837fa2113bfa5bb2d
666Dallas666 <s.mamonov.2002@yandex.ru>
2020-12-17 10:47:00 +0300

    bruh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MessengerComponent } from './components/messenger/messenger.component';
+import {HeaderComponent} from './components/header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbdCollapseNavbar } from './components/navbar/navbar.component';
+import { NgbdPopoverBasic } from './components/popout/popout.component';
+import { DialogueComponent } from './components/dialogue/dialogue.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessengerComponent,
+    HeaderComponent,
+    NgbdCollapseNavbar,
+    NgbdPopoverBasic,
+    DialogueComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HeaderComponent]
 })
 export class AppModule { }
